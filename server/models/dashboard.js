16b5f529c8cd9703056ecca6c553eeb19033318d
jdalton92 <jamesdalton7648@gmail.com>
2020-10-23 15:11:17 +1100

    update save dashboard modal, and UX when viewing/saving

--- server/models/dashboard.js
+++ server/models/dashboard.js
@@ -5,210 +5,206 @@ const occupierInvestorValues = {
   housePrice: {
     type: Number,
     required: true,
-    min: 0
+    min: 0,
   },
   deposit: {
     type: Number,
     required: true,
     min: 0,
-    max: [this.housePrice, "Deposit cannot exceed house price"]
+    max: [this.housePrice, "Deposit cannot exceed house price"],
   },
   loanType: {
     type: String,
     required: true,
-    enum: ["interestOnly", "principalAndInterest"]
+    enum: ["interestOnly", "principalAndInterest"],
   },
   interestRate: {
     type: Number,
     required: true,
     min: 0,
-    max: 100
+    max: 100,
   },
   homeloanTerm: {
     type: Number,
     required: true,
     min: 0,
-    max: [30, "Maximum term is 30 years"]
+    max: [30, "Maximum term is 30 years"],
   },
   overPayments: [
     {
       year: {
         type: Number,
-        required: true
       },
       payment: {
         type: Number,
-        required: true
-      }
-    }
+      },
+    },
   ],
   investmentPeriod: {
     type: Number,
     required: true,
     min: 0,
-    max: [30, "Maximum of 30 years"]
+    max: [30, "Maximum of 30 years"],
   },
   sellingCosts: {
     type: Number,
     required: true,
     min: 0,
-    max: 100
+    max: 100,
   },
   capitalGrowth: {
     type: Number,
     required: true,
     min: 0,
-    max: 100
+    max: 100,
   },
   inflation: {
     type: Number,
     min: 0,
-    max: 100
+    max: 100,
   },
   upfrontCosts: {
     type: Number,
     required: true,
     min: 0,
-    max: 100
+    max: 100,
   },
   recurringCosts: {
     type: Number,
     required: true,
-    min: 0
+    min: 0,
   },
   rentalYield: {
     type: Number,
     min: 0,
-    max: [100, "Cannot exceed 100%"]
+    max: [100, "Cannot exceed 100%"],
   },
   investor: {
     type: Boolean,
-    required: true
-  }
+    required: true,
+  },
 };
 
 const developerValues = {
   acquisitionPrice: {
     type: Number,
     required: true,
-    min: 0
+    min: 0,
   },
   acquisitionCosts: {
     type: Number,
     required: true,
     min: 0,
-    max: 100
+    max: 100,
   },
   dwellings: {
     type: Number,
     required: true,
-    min: 1
+    min: 1,
   },
   constructionCostPerDwelling: {
     type: Number,
     required: true,
-    min: 0
+    min: 0,
   },
   designFees: {
     type: Number,
     required: true,
-    min: 0
+    min: 0,
   },
   constructionContingency: {
     type: Number,
     required: true,
     min: 0,
-    max: 100
+    max: 100,
   },
   statutoryFees: {
     type: Number,
     required: true,
     min: 0,
-    max: 100
+    max: 100,
   },
   constructionDuration: {
     type: Number,
     required: true,
-    min: 0
+    min: 0,
   },
   planningAndDesign: {
     type: Number,
     required: true,
-    min: 0
+    min: 0,
   },
   revenuePerDwelling: {
     type: Number,
     required: true,
-    min: 0
+    min: 0,
   },
   sellingCosts: {
     type: Number,
     required: true,
     min: 0,
-    max: 100
+    max: 100,
   },
   investmentPeriod: {
     type: Number,
     required: true,
     min: 0,
-    max: [30, "Maximum 30 years investment period"]
+    max: [30, "Maximum 30 years investment period"],
   },
   recurringCosts: {
     type: Number,
     required: true,
     min: 0,
-    max: 100
+    max: 100,
   },
   rentalYield: {
     type: Number,
     required: true,
     min: 0,
-    max: 100
+    max: 100,
   },
   initialEquity: {
     type: Number,
     required: true,
-    min: 0
+    min: 0,
   },
   loanType: {
     type: String,
     required: true,
-    enum: ["interestOnly", "principalAndInterest"]
+    enum: ["interestOnly", "principalAndInterest"],
   },
   interestRate: {
     type: Number,
-    required: true
+    required: true,
   },
   loanTerm: {
     type: Number,
     required: true,
     min: 0,
-    max: [30, "Maximum 30 year term"]
+    max: [30, "Maximum 30 year term"],
   },
   overPayments: [
     {
       year: {
         type: Number,
-        required: true
       },
       payment: {
         type: Number,
-        required: true
-      }
-    }
+      },
+    },
   ],
   capitalGrowth: {
     type: Number,
     required: true,
     min: 0,
-    max: 100
+    max: 100,
   },
   constructionCostGrowth: {
     type: Number,
     required: true,
     min: 0,
-    max: 100
-  }
+    max: 100,
+  },
 };
 
 const dashboardSchema = mongoose.Schema(
@@ -216,8 +212,8 @@ const dashboardSchema = mongoose.Schema(
     type: {
       type: String,
       required: true,
-      enum: ["developer", "occupierInvestor"]
-    }
+      enum: ["developer", "occupierInvestor"],
+    },
   },
   { discriminatorKey: "type" }
 );
@@ -227,21 +223,21 @@ const schema = mongoose.Schema({
   description: {
     type: String,
     minlength: 3,
-    required: true
+    required: true,
   },
   address: {
     type: String,
     required: true,
-    minlength: 3
+    minlength: 3,
   },
   date: {
     type: Date,
-    required: true
+    required: true,
   },
   user: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: "User"
-  }
+    ref: "User",
+  },
 });
 
 // Add values fields to schema dependant on what type
